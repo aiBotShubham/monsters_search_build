26c5539ad99f72d1c8cfebaf45b681c4a02f5571
aiBotShubham <shubhamgandhi65@gmail.com>
2019-07-29 14:15:18 +0530

    first commit

---
+++ precache-manifest.6767be35ace8f3c82fa60c159a9664a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5fcb17a7aa780b5ab1be8a8e732f1a",
+    "url": "/monsters_search_REACT/index.html"
+  },
+  {
+    "revision": "ef3483cfab3ac6c399a0",
+    "url": "/monsters_search_REACT/static/css/main.1a2c2bbd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters_search_REACT/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ef3483cfab3ac6c399a0",
+    "url": "/monsters_search_REACT/static/js/main.9776d814.chunk.js"
+  },
+  {
+    "revision": "7e6d4909df92eee13a93",
+    "url": "/monsters_search_REACT/static/js/runtime~main.31edddab.js"
+  }
+]);
---
+++ precache-manifest.f2f30d57eda1efd29f902925ea0221a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae3febda51f9c1697a403bcfb46b4c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43a8004aee010471a556",
+    "url": "/static/css/main.1a2c2bbd.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "43a8004aee010471a556",
+    "url": "/static/js/main.c9ba6664.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
